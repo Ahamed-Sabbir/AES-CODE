91742fe2fc32266713010bd0898d6a9f857659ed
Ahamed-Sabbir <96658618+Ahamed-Sabbir@users.noreply.github.com>
2023-05-21 09:38:16 +0600

    Update AES.cpp

--- AES.cpp
+++ AES.cpp
@@ -236,7 +236,7 @@ int main(){
     ADD_ROUND_KEY();
 
   }
-  cout << "Encrypted:\n";
+  cout << "Encrypted: \n";
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 4; j++){
       cout<<plainText[i][j]<<" ";
